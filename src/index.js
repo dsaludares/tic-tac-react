9b6057a298b941f70c0bdb85bccad70464643e6a
Denise Saludares <denise.saludares@ucsb.edu>
2018-07-20 00:40:56 -0700

    Map over history in Game’s render method

--- src/index.js
+++ src/index.js
@@ -78,6 +78,17 @@ handleClick(i) {
   const current = history[history.length - 1];
   const winner = calculateWinner(current.squares);
 
+  const moves = history.map((StereoPannerNode, move) => {
+    const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li>
+        <button onClick={() => this.jumptTo(move)}>{desc}</button>
+      </li>
+    );
+  });
+
   let status;
   if (winner) {
     status = <div>
@@ -101,7 +112,7 @@ handleClick(i) {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
